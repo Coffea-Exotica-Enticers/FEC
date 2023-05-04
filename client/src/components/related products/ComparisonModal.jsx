d347334563676d93c5e534df4c269090b251ec5f
Benjamin Pak <benjaminjpak95@gmail.com>
2023-05-04 11:01:39 -0700

    Created the skeleton of the product related page

---
+++ client/src/components/related products/ComparisonModal.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+
+const ComparisonModal = () => {
+
+  return (
+
+  )
+}
+
+export default ComparisonModal;
---
+++ client/src/components/related products/RelatedProductCard.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import ComparisonModal from './ComparisonModal.jsx';
+
+const RelatedProductCard = () => {
+
+  return (
+    <div>
+      <h1>Related Product</h1>
+    </div>
+  )
+}
+
+export default RelatedProductCard;
