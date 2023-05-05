abb72b063369ef8939fbd77ae860f28cd476868a
davidguy3237 <davidtran3237@gmail.com>
2023-05-05 00:16:08 -0700

    Import axios and destructure env variables in every controller

--- server/controllers/interactions.js
+++ server/controllers/interactions.js
@@ -1,3 +1,7 @@
+const axios = require('axios');
+
+const { ATELIER_API, API_TOKEN } = process.env;
+
 module.exports = {
   post(req, res) {},
 };
--- server/controllers/reviews.js
+++ server/controllers/reviews.js
@@ -1,3 +1,7 @@
+const axios = require('axios');
+
+const { ATELIER_API, API_TOKEN } = process.env;
+
 module.exports = {
   get(req, res) {},
   post(req, res) {},
