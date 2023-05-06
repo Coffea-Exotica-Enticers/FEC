b7eaf5f3307f6c5b3ab46c166c9c386b92de37ce
Benjamin Pak <benjaminjpak95@gmail.com>
2023-05-06 16:17:57 -0700

    Updated useEffect Get request

--- client/src/components/related-products/RelatedProductsList.jsx
+++ client/src/components/related-products/RelatedProductsList.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable max-len */
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import RelatedProductCard from './RelatedProductCard';
@@ -6,15 +5,11 @@ import RelatedProductCard from './RelatedProductCard';
 function RelatedProductsList({ product }) {
   const [relatedProducts, setRelatedProducts] = useState([]);
 
-  const getRelated = () => {
-    axios.get(`/products/${product.id}/related`)
-      .then((results) => setRelatedProducts(results))
-      .catch((err) => console.error('Error retrieving item data', err));
-  };
-
   useEffect(() => {
     if (product) {
-      getRelated();
+      axios.get(`/products/${product.id}/related`)
+        .then(({ data }) => setRelatedProducts(data))
+        .catch((err) => console.error('Error retrieving item data', err));
     }
   }, [product]);
 
@@ -22,7 +17,11 @@ function RelatedProductsList({ product }) {
     <div>
       <h1>Related Products List</h1>
       <div className="rp-list">
-        {relatedProducts.length ? relatedProducts.map((item) => <RelatedProductCard key={item.id} item={item} />) : <div>No available items</div>}
+        {
+          relatedProducts.length
+            ? relatedProducts.map((item) => <RelatedProductCard key={item.id} item={item} />)
+            : 'Loading...'
+        }
       </div>
     </div>
   );
