43dbeab5005f574df86a5046e62c48afb0bbcfbf
davidguy3237 <davidtran3237@gmail.com>
2023-05-06 15:21:53 -0700

    Add basic styling and formatting for ReviewTiles

--- client/src/components/ratings-and-reviews/ReviewTile.jsx
+++ client/src/components/ratings-and-reviews/ReviewTile.jsx
@@ -4,39 +4,45 @@ import StarRatings from '../shared/StarRatings';
 export default function ReviewTile({ review }) {
   return (
     <div className="review-tile">
+      <div className="top-row">
+        <div className="top-row-left">
+          <StarRatings rating={review.rating} />
+        </div>
+        <div className="top-row-right">
+          {`${review.reviewer_name}, `}
+          {new Date(review.date).toDateString()}
+        </div>
+      </div>
+      <div className="review-summary">
+        {review.summary}
+      </div>
+      <div className="review-body">
+        {review.body}
+      </div>
       {
-        review
+      review.recommend
+        ? <div className="recommend-label">&#x2713; I recommend this product</div>
+        : null
+      }
+      {
+        review.response
           ? (
-            <div>
-              <StarRatings rating={review.rating} />
-              <div>
-                Date:
-                {new Date(review.date).toDateString()}
-              </div>
-              <div>
-                Summary:
-                {review.summary}
-              </div>
-              <div>
-                body:
-                {review.body}
-              </div>
-              <div>
-                Recommend:
-                {`${review.recommend}`}
-              </div>
-              <div>
-                Name:
-                {review.reviewer_name}
-              </div>
-              <div>
-                Helpful?:
-                {review.helpfulness}
-              </div>
+            <div className="review-response">
+              <div className="review-response-header">Response from seller:</div>
+              {review.response}
             </div>
           )
-          : 'Loading...'
+          : null
       }
+      <div className="review-bottom-row">
+        Helpful?
+        <button type="button" className="review-helpful">Yes</button>
+        <span className="review-helpfulness-display">
+          (
+          {review.helpfulness}
+          )
+        </span>
+      </div>
     </div>
   );
 }
