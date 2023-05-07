6748e19e66ad26ed0c81767f5a9dc6d73cec5c6a
Benjamin Pak <benjaminjpak95@gmail.com>
2023-05-06 17:32:42 -0700

    Implemented getMeta method in reviews controller

--- server/controllers/reviews.js
+++ server/controllers/reviews.js
@@ -18,6 +18,21 @@ module.exports = {
         res.status(404).json(err);
       });
   },
+  getMeta(req, res) {
+    axios.get(`${ATELIER_API}/reviews/meta`, {
+      params: {
+        product_id: req.query.product_id,
+      },
+      headers: {
+        authorization: API_TOKEN,
+      },
+    })
+      .then(({ data }) => res.json(data.characteristics))
+      .catch((err) => {
+        console.log('ERROR GETTING META DATA', err);
+        res.status(404).json(err);
+      });
+  },
   post(req, res) {},
   putHelpful(req, res) {},
   putReport(req, res) {},
