f98dd92a3c5c34936cb2ab436e7c907dfd38c746
Benjamin Pak <benjaminjpak95@gmail.com>
2023-05-08 12:44:01 -0700

    Implemented axios.all request for RelatedProductCard

--- client/src/components/related-products/RelatedProductCard.jsx
+++ client/src/components/related-products/RelatedProductCard.jsx
@@ -1,10 +1,55 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import ComparisonModal from './ComparisonModal';
 
-function RelatedProductCard() {
+function RelatedProductCard({ item }) {
+  const [product, setProduct] = useState({});
+  const [rating, setRating] = useState({});
+
+  const styles = axios.get(`./products/${item.id}/styles`);
+  const starRating = axios.get(`./reviews/meta/${item.id}`);
+
+  useEffect(() => {
+    axios.all([styles, starRating]).then(axios.spread((prod, star) => {
+      console.log('prod: ', prod.data, 'star: ', star);
+      setProduct(prod.data);
+      setRating(star.data);
+    }));
+  }, []);
+
+  // useEffect(() => {
+  //   axios.get(`./products/${item.id}/styles`)
+  //     .then(({ data }) => {
+  //       console.log('DATA', data)
+  //       setProduct(data)})
+  //     .catch((err) => console.error('Error retrieving item style', err));
+  // }, []);
+
+  console.log('item', item)
+
   return (
-    <div>
-      <h1>Related Product</h1>
+    <div className="rp-card">
+      <button type="button">star</button>
+      <div className="preview">
+        product preview
+      </div>
+
+      <div className="category">
+        {item.category}
+      </div>
+
+      <div className="description">
+        <p><em>{item.name}</em></p>
+        <p>
+          Price:
+          {item.default_price}
+        </p>
+        <p>
+          Product Description:
+          {item.description}
+        </p>
+      </div>
+
     </div>
   );
 }
