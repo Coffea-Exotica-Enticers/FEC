d33bcd61e5bc986950c63d61a8ef6396d20701ae
davidguy3237 <davidtran3237@gmail.com>
2023-05-08 12:37:22 -0700

    Update eslint to include react hooks, jest, and testing library

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,21 @@ module.exports = {
     browser: true,
     es2021: true,
     node: true,
+    'jest/globals': true,
   },
   extends: [
     'plugin:react/recommended',
     'airbnb',
+    'airbnb/hooks',
   ],
   overrides: [
+    {
+      files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+      extends: [
+        'plugin:jest/recommended',
+        'plugin:testing-library/react',
+      ],
+    },
   ],
   parserOptions: {
     ecmaVersion: 'latest',
@@ -16,6 +25,8 @@ module.exports = {
   },
   plugins: [
     'react',
+    'jest',
+    'testing-library',
   ],
   rules: {
     'react/prop-types': 0,
