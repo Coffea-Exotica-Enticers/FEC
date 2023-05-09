c1760c0776a49876bf33d705296d0c9ac74ebb12
Benjamin Pak <benjaminjpak95@gmail.com>
2023-05-09 14:21:23 -0700

    Created rp test file

---
+++ client/src/components/related-products/__test__/rp.test.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import {cleanup, fireEvent, render} from '@testing-library/react';
+import RelatedProductCard from '../RelatedProductCard';
+import RelatedProductsList from '../RelatedProductsList';
+
