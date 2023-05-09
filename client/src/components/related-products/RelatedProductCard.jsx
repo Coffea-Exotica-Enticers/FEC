7abaa856633000d8736cb383299d291434511fb3
Benjamin Pak <benjaminjpak95@gmail.com>
2023-05-08 17:02:08 -0700

    Implemented getAverageRating function and axios GET request for ProductCard

--- client/src/components/related-products/RelatedProductCard.jsx
+++ client/src/components/related-products/RelatedProductCard.jsx
@@ -3,32 +3,38 @@ import axios from 'axios';
 import ComparisonModal from './ComparisonModal';
 
 function RelatedProductCard({ item }) {
-  const [product, setProduct] = useState({});
-  const [rating, setRating] = useState({});
+  const [productStyles, setProductStyles] = useState({});
+  const [rating, setRating] = useState(null);
 
-  const styles = axios.get(`./products/${item.id}/styles`);
-  const starRating = axios.get(`./reviews/meta/${item.id}`);
+  // Function to find the average star rating
+  function getAverageRating(ratings) {
+    const starRatings = Object.values(ratings);
+    let sumOfRatings = 0;
+    let totalNumOfRatings = 0;
 
+    for (let i = 0; i < starRatings.length; i += 1) {
+      sumOfRatings += starRatings[i] * i + 1;
+      totalNumOfRatings += Number(starRatings[i]);
+    }
+    const averageRating = sumOfRatings / totalNumOfRatings;
+    return averageRating.toFixed(1);
+  }
+
+  // Define multiple endpoints
+  const styles = axios.get(`/products/${item.id}/styles`);
+  const starRating = axios.get('/reviews/meta', { params: { product_id: item.id } });
+
+  // Sends a GET request for item styles and star rating on render
   useEffect(() => {
     axios.all([styles, starRating]).then(axios.spread((prod, star) => {
-      console.log('prod: ', prod.data, 'star: ', star);
-      setProduct(prod.data);
-      setRating(star.data);
-    }));
+      setProductStyles(prod.data);
+      setRating(getAverageRating(star.data));
+    }))
+      .catch((err) => console.error('There was an error retrieving styles or rating data: ', err));
   }, []);
 
-  // useEffect(() => {
-  //   axios.get(`./products/${item.id}/styles`)
-  //     .then(({ data }) => {
-  //       console.log('DATA', data)
-  //       setProduct(data)})
-  //     .catch((err) => console.error('Error retrieving item style', err));
-  // }, []);
-
-  console.log('item', item)
-
   return (
-    <div className="rp-card">
+    <div className="rp-card" style={{ display: 'column', flexDirection: 'row' }}>
       <button type="button">star</button>
       <div className="preview">
         product preview
@@ -40,14 +46,12 @@ function RelatedProductCard({ item }) {
 
       <div className="description">
         <p><em>{item.name}</em></p>
-        <p>
-          Price:
-          {item.default_price}
-        </p>
-        <p>
-          Product Description:
-          {item.description}
-        </p>
+        <p>Price: {item.default_price}</p>
+        <p>Product Description: {item.description}</p>
+      </div>
+
+      <div className="rating">
+        <p>Star Rating: {rating}</p>
       </div>
 
     </div>
