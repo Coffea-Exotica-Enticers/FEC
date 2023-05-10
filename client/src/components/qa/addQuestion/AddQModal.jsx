053271c0e8ef75a4e09ba5103207989b2fca0ea6
Yoko 8 <yo.ko.8.eight@gmail.com>
2023-05-10 14:03:12 -0700

    Implemented modal window. Need to send axios request to database next.

--- client/src/components/qa/addQuestion/AddQModal.jsx
+++ client/src/components/qa/addQuestion/AddQModal.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from 'react-modal';
 
-export default function AddQModal({ show, hideModal }) {
-  const showHideClassName = show ? "modal display-block" : "modal display-none";
+export default function AddQModal({ show, setShow }) {
+  const [qEntry, setQEntry] = useState('');
+  const [nameEntry, setNameEntry] = useState('');
+  const [emailEntry, setEmailEntry] = useState('');
 
   return (
-    <div className={showHideClassName}>
-      <div>This is a Modal</div>
-      <button onClick={hideModal}>Close</button>
-    </div>
+    <Modal isOpen={show} onRequestClose={() => setShow(false)}>
+      <h1>Ask Your Question</h1>
+      <button onClick={() => setShow(false)}>Close</button>
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        setShow(false);
+      }}
+      >
+        <textarea placeholder="Write Your Question Here..." onChange={(e) => setQEntry(e.target.value)}></textarea>
+        <input placeholder="Nickname..." onChange={(e) => setNameEntry(e.target.value)}></input>
+        <input placeholder="Email..." onChange={(e) => setEmailEntry(e.target.value)}></input>
+        <button type="submit">Submit</button>
+      </form>
+    </Modal>
   )
 }
--- client/src/components/qa/addQuestion/AddQuestion.jsx
+++ client/src/components/qa/addQuestion/AddQuestion.jsx
@@ -4,19 +4,10 @@ import AddQModal from './AddQModal';
 export default function AddQuestion() {
   const [show, setShow] = useState(false);
 
-  function showModal() {
-    console.log('I happened');
-    setShow(true);
-  }
-
-  function hideModal() {
-    setShow(false);
-  }
-
   return (
     <>
-      <button type="button" onClick={showModal}>Ask Your Question</button>
-      <AddQModal show={show} hideModal={hideModal} />
+      <button type="button" onClick={() => setShow(true)}>Ask Your Question</button>
+      <AddQModal show={show} setShow={setShow} />
     </>
   );
 }
