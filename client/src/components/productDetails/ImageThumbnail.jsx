cf4d26239f571afb08ae4d34d758fb83cbfbf429
Segue <gencogluseguy@gmail.com>
2023-05-11 12:07:12 -0700

    Thumbnails are rendering for Image component

--- client/src/components/productDetails/ImageThumbnail.jsx
+++ client/src/components/productDetails/ImageThumbnail.jsx
@@ -1,17 +1,22 @@
 import React, { useState, useEffect } from 'react';
 
 export default function ImageThumbnail({ selectedStyle }) {
+  const thumbnails = selectedStyle.photos;
   return (
     <div className="ThumbnailContainer">
-      <ul className="VerticalSlider">
-        <li className="thumb-gallery">
-          <button type="button">
-            <div className="image withPlaceholder">
-              <img className="isLoaded" src={selectedStyle.photos[0].thumbnail_url} alt="gallery" />
-            </div>
-          </button>
-        </li>
-      </ul>
+      <div className="Thumbnail">
+        {thumbnails.map((thumbnail) => (
+          <ul className="VerticalSlider">
+            <li className="thumb-gallery">
+              <button type="button">
+                <div className="image withPlaceholder">
+                  <img className="isLoaded" src={thumbnail.thumbnail_url} alt="gallery" />
+                </div>
+              </button>
+            </li>
+          </ul>
+        ))}
+      </div>
     </div>
 
   );
