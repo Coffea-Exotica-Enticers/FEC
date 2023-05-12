ab645fc1c9acdd324e1323859a47af7d99dd1f3e
davidguy3237 <davidtran3237@gmail.com>
2023-05-12 13:59:08 -0700

    Implement basic sort reviews functionality

--- client/src/components/ratings-and-reviews/reviews/ReviewsList.jsx
+++ client/src/components/ratings-and-reviews/reviews/ReviewsList.jsx
@@ -5,54 +5,65 @@ import ReviewTile from './ReviewTile';
 const { useState, useEffect } = React;
 
 export default function ReviewsList({ product }) {
-  const [reviews, setReviews] = useState([]);
-  const [page, setPage] = useState(3);
-  const [nextPage, setNextPage] = useState([]);
+  const [shownReviews, setShownReviews] = useState([]);
+  const [showCount, setShowCount] = useState(4);
+  const [reviewsCache, setReviewsCache] = useState([]);
+
   useEffect(() => {
     if (product) {
       axios.get('/reviews', {
         params: {
-          page: 1,
+          count: 1000,
           product_id: product.id,
+          sort: 'relevant',
         },
       })
-        .then(({ data }) => setReviews(data))
-        .then(() => axios.get('/reviews', {
-          params: {
-            page: 2,
-            product_id: product.id,
-          },
-        }))
-        .then(({ data }) => setNextPage(data));
+        .then(({ data }) => {
+          setShownReviews(data.slice(0, 2));
+          setReviewsCache(data);
+        });
     }
   }, [product]);
 
-  const showMore = () => {
+  const showMoreReviews = () => {
+    setShownReviews(reviewsCache.slice(0, showCount));
+    setShowCount(showCount + 2);
+  };
+
+  const sort = (e) => {
     axios.get('/reviews', {
       params: {
-        page,
+        count: 1000,
         product_id: product.id,
+        sort: e.target.value,
       },
     })
       .then(({ data }) => {
-        setReviews((currentReviews) => [...currentReviews, ...nextPage]);
-        setNextPage(data);
-        setPage(page + 1);
+        setShownReviews(data.slice(0, shownReviews.length));
+        setReviewsCache(data);
       });
   };
 
   if (!product) return <div className="reviews-list">Loading...</div>;
-  if (product && !reviews.length) return <div className="reviews-list">Add a review!</div>;
-  if (product && reviews.length) {
+  if (product && !shownReviews.length) return <div className="reviews-list">Add a review!</div>;
+  if (product && shownReviews.length) {
     return (
       <div className="reviews-list">
+        <div className="reviews-sort">
+          {`${reviewsCache.length} reviews, sorted by`}
+          <select name="sort" id="reviews-sort-selector" onChange={sort}>
+            <option value="relevant" selected>Relevance</option>
+            <option value="newest">Newest</option>
+            <option value="helpful">Helpful</option>
+          </select>
+        </div>
         <div className="reviews-container">
-          {reviews.map((review) => <ReviewTile key={review.review_id} review={review} />)}
+          {shownReviews.map((review) => <ReviewTile key={review.review_id} review={review} />)}
         </div>
         {
-          nextPage.length
+          shownReviews.length < reviewsCache.length
             ? (
-              <button type="button" className="more-reviews" onClick={showMore}>
+              <button type="button" className="more-reviews" onClick={showMoreReviews}>
                 More Reviews
               </button>
             )
