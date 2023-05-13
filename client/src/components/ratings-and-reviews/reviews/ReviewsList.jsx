c6f9b34f1167df5e5bc4db27aeadfa91c0193411
davidguy3237 <davidtran3237@gmail.com>
2023-05-12 18:01:35 -0700

    Refactor sort & search filters so they work together

--- client/src/components/ratings-and-reviews/reviews/ReviewsList.jsx
+++ client/src/components/ratings-and-reviews/reviews/ReviewsList.jsx
@@ -8,6 +8,7 @@ export default function ReviewsList({ product }) {
   const [shownReviews, setShownReviews] = useState([]);
   const [showCount, setShowCount] = useState(2);
   const [reviewsCache, setReviewsCache] = useState([]);
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
     if (product) {
@@ -18,18 +19,21 @@ export default function ReviewsList({ product }) {
           sort: 'relevant',
         },
       })
-        .then(({ data }) => {
-          setShownReviews(data.slice(0, 2));
-          setReviewsCache(data);
-        })
+        .then(({ data }) => setReviewsCache(data))
         .catch((err) => console.error('ERROR GETTING REVIEWS', err));
     }
   }, [product]);
 
-  const showMoreReviews = () => {
-    setShowCount(showCount + 2);
-    setShownReviews(reviewsCache.slice(0, showCount + 2));
-  };
+  useEffect(() => {
+    if (search.length < 3) {
+      setShownReviews(reviewsCache.slice(0, showCount));
+    } else {
+      setShownReviews(
+        reviewsCache.filter((review) => review.summary.toLowerCase().includes(search.toLowerCase())
+        || review.body.toLowerCase().includes(search.toLowerCase())).slice(0, showCount),
+      );
+    }
+  }, [showCount, reviewsCache, search]);
 
   const sort = (e) => {
     axios.get('/reviews', {
@@ -39,23 +43,8 @@ export default function ReviewsList({ product }) {
         sort: e.target.value,
       },
     })
-      .then(({ data }) => {
-        setShownReviews(data.slice(0, shownReviews.length));
-        setReviewsCache(data);
-      })
+      .then(({ data }) => setReviewsCache(data))
       .catch((err) => console.error('ERROR GETTING & SORTING REVIEWS', err));
-  };
-
-  const searchReviews = (e) => {
-    const search = e.target.value;
-    if (search.length < 3) {
-      setShownReviews(reviewsCache.slice(0, showCount));
-    } else {
-      setShownReviews(
-        reviewsCache.filter((review) => review.summary.toLowerCase().includes(search.toLowerCase())
-        || review.body.toLowerCase().includes(search.toLowerCase())).slice(0, showCount),
-      );
-    }
   };
 
   if (!product) return <div className="reviews-list">Loading...</div>;
@@ -64,7 +53,12 @@ export default function ReviewsList({ product }) {
     return (
       <div className="reviews-list">
         <div className="reviews-search">
-          <input type="text" className="reviews-search-bar" placeholder="Search reviews..." onChange={searchReviews} />
+          <input
+            type="text"
+            className="reviews-search-bar"
+            placeholder="Search reviews..."
+            onChange={(e) => setSearch(e.target.value)}
+          />
         </div>
         <div className="reviews-sort">
           {`${reviewsCache.length} reviews, sorted by`}
@@ -80,7 +74,11 @@ export default function ReviewsList({ product }) {
         {
           shownReviews.length < reviewsCache.length
             ? (
-              <button type="button" className="more-reviews" onClick={showMoreReviews}>
+              <button
+                type="button"
+                className="more-reviews"
+                onClick={() => setShowCount(showCount + 2)}
+              >
                 More Reviews
               </button>
             )
